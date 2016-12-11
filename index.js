26b62479b55fd958a157a2e6d33479dd4bcce490
Liz Shaw <eshaw@hubspot.com>
2016-12-11 16:55:06 -0500

    Adjust port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const Expression = require('./expression');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const server = app.listen(8765, () => { console.log('Express server listening on port %d in %s mode', server.address().port,   app.settings.env);});
+const server = app.listen(process.env.PORT, () => { console.log('Express server listening on port %d in %s mode', server.address().port,   app.settings.env);});
 
 app.post('/roll', (req, res) => {
   const text = req.body.text;
